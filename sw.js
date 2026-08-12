40381066b2d85ec9ef1ce2a82791c4011888dd45
burkart350z-coder <burkart350z@gmail.com>
2026-08-12 21:12:09 +0200

    Refresh service worker cache for v25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="meinarchiv-v15";
-const APP=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="meinarchiv-v25";
+const APP=["./","./index.html","./styles.css","./app.js","./preview-fix.js","./powerpay-fix.js","./archive-tools.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match("./index.html")))});
